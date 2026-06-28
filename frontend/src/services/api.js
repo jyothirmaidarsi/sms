0d2a2966a9265724074eccb567fb3733afdb3715
Jyothirmai Darsi <jyothirmaidarsi@gmail.com>
2026-06-28 12:02:40 +0530

    api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://sms-0789.onrender.com/api"
 });
 
 // Auto attach token to every request
